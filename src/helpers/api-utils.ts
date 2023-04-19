a64467aba0e5cda7f2098da6c825b0f521e20bcb
Noelia Lopez <noelia.l@toptal.com>
2023-04-19 15:54:19 +0200

    Updated connection string

--- src/helpers/api-utils.ts
+++ src/helpers/api-utils.ts
@@ -2,19 +2,19 @@ import { ObjectId } from 'mongodb'
 import { TodoItem } from '../models/todoItem'
 
 export function getAllTodos(): Promise<TodoItem[]> {
-	return fetch('http://localhost:3000/api/todo', {
+	return fetch('/api/todo', {
 		method: 'GET'
 	}).then(res => res.json())
 }
 
 export function getTodoById(id: string): Promise<TodoItem> {
-	return fetch(`http://localhost:3000/api/todo?id=${id}`, {
+	return fetch(`/api/todo?id=${id}`, {
 		method: 'GET'
 	}).then(res => res.json())
 }
 
 export function addTodo(todo: TodoItem) {
-	return fetch('http://localhost:3000/api/todo', {
+	return fetch('/api/todo', {
 		method: 'POST',
 		body: JSON.stringify(todo),
 		headers: {
@@ -24,7 +24,7 @@ export function addTodo(todo: TodoItem) {
 }
 
 export function updateTodo(todo: TodoItem) {
-	return fetch('http://localhost:3000/api/todo', {
+	return fetch('/api/todo', {
 		method: 'PUT',
 		body: JSON.stringify(todo),
 		headers: {
@@ -34,7 +34,7 @@ export function updateTodo(todo: TodoItem) {
 }
 
 export function deleteTodo(id: ObjectId) {
-	return fetch(`http://localhost:3000/api/todo?id=${id.toString()}`, {
+	return fetch(`/api/todo?id=${id.toString()}`, {
 		method: 'DELETE'
 	}).then(res => res.json())
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,10 @@
 import { MongoClient, MongoClientOptions } from 'mongodb'
 
-const uri = process.env.NEXT_PUBLIC_MONGODB_URI as string
+const username = process.env.NEXT_PUBLIC_MONGODB_USERNAME
+const password = process.env.NEXT_PUBLIC_MONGODB_PASSWORD
+const cluster = process.env.NEXT_PUBLIC_MONGODB_CLUSTER
+
+const connectionString = `mongodb+srv://${username}:${password}@${cluster}.0w2evi4.mongodb.net/?retryWrites=true&w=majority`
 
 const mongoOptions = {
 	useUnifiedTopology: true,
@@ -15,18 +19,18 @@ const options: MongoClientOptions = {
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
-if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
+if (!username || !password || !cluster) {
 	throw new Error('Add Mongo URI to .env.local')
 }
 
 if (process.env.NODE_ENV === 'development') {
 	if (!global._mongoClientPromise) {
-		client = new MongoClient(uri, options)
+		client = new MongoClient(connectionString, options)
 		global._mongoClientPromise = client.connect()
 	}
 	clientPromise = global._mongoClientPromise
 } else {
-	client = new MongoClient(uri, options)
+	client = new MongoClient(connectionString, options)
 	clientPromise = client.connect()
 }
 
