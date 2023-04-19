7b30a50250f3336a96ec1331c7e0bb6b8d75f69f
Noelia Lopez <noelia.l@toptal.com>
2023-04-19 11:40:59 +0200

    Deleted dummy data file

--- data/dummy-data.tsx
+++
@@ -1,5 +0,0 @@
-import { TodoItem } from "../src/models/todoItem";
-
-export function getFilteredTodoItems(todos: TodoItem[], category: string, completed: boolean): TodoItem[] {
-	return todos.filter((todo) => todo.category === category && todo.completed === completed)
-}
